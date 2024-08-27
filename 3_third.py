7d666339cf88b3ccc6a5791862fe87ece1c13568
VedantP45 <paratkarvedant45@gmail.com>
2024-08-27 13:22:06 +0530

    Add files via upload

---
+++ 3_third.py
@@ -0,0 +1,15 @@
+def is_isogram(s):
+    clean_string = s.lower().replace(" ", "").replace("-", "")
+
+    char_set = set(clean_string)
+
+    return len(char_set) == len(clean_string)
+
+# Test cases
+x = str(input('Enter string: '))
+
+print(is_isogram(x))  
+# print(is_isogram("uncopyrightable"))  
+# print(is_isogram("First# Clan!")) 
+# print(is_isogram("BackGround"))  
+# print(is_isogram("isograms")) 
